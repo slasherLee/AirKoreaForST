246f64d2e49296e823c182dc5a683a3de793eab1
Seung-Cheol Lee <Seung-Cheol.Lee@viavisolutions.com>
2018-03-15 18:17:47 +0900

    modify parsing json logic

--- devicetypes/slasherLee/airkoreaforst.src/dth_airkorea.groovy
+++ devicetypes/slasherLee/airkoreaforst.src/dth_airkorea.groovy
@@ -14,11 +14,7 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
- preferences {
-        input "station", "string", title: "Select your station name", defaultValue: "", required: true, displayDuringSetup: true
-        input "refreshRate", "enum", title: "Data refresh rate", defaultValue: 0, options:[0: "Never" ,10: "Every 10 Minutes", 30: "Every 1/2 hour", 60 : "Every Hour", 240 :"Every 4 hours",
-        	360: "Every 6 hours", 720: "Every 12 hours", 1440: "Once a day"], displayDuringSetup: true
-}
+
 metadata {
 	definition (name: "AirKorea", namespace: "slasehrLee", author: "SeungCheol Lee") {
 		capability "Polling"
@@ -29,9 +25,13 @@ metadata {
         attribute "pm10_value", "number"
         attribute "pm25_value", "number"
         attribute "o3_value", "number"
-        attribute "station_name", "String"
         attribute "data_time", "String"
 	}
+    preferences {
+        input "station_name", "text", title: "station name", description: "The station name of your region", required: true
+        input "refreshRate", "enum", title: "Data refresh rate", defaultValue: 0, options:[0: "Never" ,10: "Every 10 Minutes", 30: "Every 1/2 hour", 60 : "Every Hour", 240 :"Every 4 hours",
+        	360: "Every 6 hours", 720: "Every 12 hours", 1440: "Once a day"], displayDuringSetup: true
+    }
 	simulator {
 		// TODO: define status and reply messages here
 	}
@@ -88,7 +88,7 @@ metadata {
 	}
 }
 private getAPIKey() {
-    return "ENTER YOUR API KEY HERE (KEEP THE QUOTATION MARKS)"
+    return "8OAJRHK5%2BTn75SiXMtbgUvkVSWOfBvxYSmFZ6J9NMZsBNbsWwvLH7dPFg04%2FTqSiDYh00l181QE91qw4e1dNmw%3D%3D"
 }
 def parse(String description) {
 	log.debug "Parsing '${description}'"
@@ -98,6 +98,7 @@ def refresh() {
 }
 // handle commands
 def poll() {
+    def station_name = "´Ü´ëµ¿"
     if (station_name){
         def refreshTime =  refreshRate ? (refreshRate as int) * 60 : 0
         if (refreshTime > 0) {
@@ -106,10 +107,13 @@ def poll() {
         }
         else log.debug "Data will never repoll" 
         def accessKey = getAPIKey()
-        def url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=" + urlEncode(station_name) + 
-                  "&dataTerm=month&pageNo=1&numOfRows=1&ServiceKey=${accessKey}&_returnType=json"
+        def params = [
+    	    uri: "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=" + /*urlEncode(station_name)*/ station_name + 
+                  "&dataTerm=month&pageNo=1&numOfRows=1&ServiceKey=${accessKey}&_returnType=json",
+        	contentType: 'application/json'
+    	]
         try {
-            httpGet(url) {resp ->
+            httpGet(params) {resp ->
                 resp.headers.each {
                     log.debug "${it.name} : ${it.value}"
                 }
@@ -117,42 +121,40 @@ def poll() {
                 log.debug "response contentType: ${resp.contentType}"
                 // get the status code of the response
                 log.debug "response status code: ${resp.status}"
-                if (resp.status==200){
+                if (resp.status == 200){
                     // get the data from the response body
-                    log.debug "response data: ${resp.data}"
-                    
-                    if( resp.data.list[1].pm10_value )
+                    //log.debug "response data: ${resp.data}"
+              
+                    if( resp.data.list[0].pm10Value )
                     {
-                        log.debug "PM10: ${resp.data.list[1].pm10Value}"
-                        sendEvent(name: "pm10_value", value: resp.data.list[1].pm10Value as Integer, unit: "¥ìg/m©ø", isStateChange: true)
+                        log.debug "PM10: ${resp.data.list[0].pm10Value}"
+                        sendEvent(name: "pm10_value", value: resp.data.list[0].pm10Value as Integer, unit: "¥ìg/m©ø", isStateChange: true)
                     }
                     else
                         sendEvent(name: "pm10_value", value: -1, unit: "", isStateChange: true)
 
 
-                    if( resp.data.list[1].pm25_value )
+                    if( resp.data.list[0].pm25Value )
                     { 
-                        log.debug "PM25: ${resp.data.list[1].pm25Value}"
-                        sendEvent(name: "pm25_value", value: resp.data.list[1].pm25Value as Integer, unit: "¥ìg/m©ø", isStateChange: true)
+                        log.debug "PM25: ${resp.data.list[0].pm25Value}"
+                        sendEvent(name: "pm25_value", value: resp.data.list[0].pm25Value as Integer, unit: "¥ìg/m©ø", isStateChange: true)
                     }
                     else
                         sendEvent(name: "pm25_value", value: -1, unit: "", isStateChange: true)
 
                      
-                    if( resp.data.list[1].o3_value )
+                    if( resp.data.list[0].o3Value )
                     {
-                        log.debug "Ozone: ${resp.data.list[1].o3Value}"
-                        sendEvent(name: "o3_value", value: resp.data.list[1].o3Value as Double, unit: "ppm", isStateChange: true)
+                        log.debug "Ozone: ${resp.data.list[0].o3Value}"
+                        sendEvent(name: "o3_value", value: resp.data.list[0].o3Value as Double, unit: "ppm", isStateChange: true)
                     }
                     else
                         sendEvent(name: "o3_value", value: -1, unit: "", isStateChange: true)
 
-                    
-                    log.debug "Khai value: ${resp.data[1].khaiValue}"
-                    sendEvent(name: "khai_value", value: resp.data.list[1].khaiValue as Integer, unit: "", isStateChange: true)
+                    log.debug "Khai value: ${resp.data[0].khaiValue}"
+                    sendEvent(name: "khai_value", value: resp.data.list[0].khaiValue as Integer, unit: "", isStateChange: true)
 
-                    def khai_grade = resp.data.list[1].khaiGrade
-                    
+                    def khai_grade = resp.data.list[0].khaiGrade
                     def khai_text 
                     if (khai_grade == 0) khai_text="GREAT"
                     else if (allpollu == 1) khai_text="GOOD"
@@ -160,7 +162,7 @@ def poll() {
                     else if (allpollu == 3) khai_text="POOR"
                     else if (allpollu == 4) khai_text="VERY POOR"
 
-                    sendEvent(name:"data_time", value:khai_text + "(${resp.data.parm.stationName})" + " - Last Updated: " + resp.data.list[1].dataTime, isStateChange: true)
+                    sendEvent(name:"data_time", value:khai_text + "(${resp.data.parm.stationName})" + " - Last Updated: " + resp.data.list[0].dataTime, isStateChange: true)
           		}
             	else if (resp.status==429) log.debug "You have exceeded the maximum number of refreshes today"	
                 else if (resp.status==500) log.debug "Internal server error"
